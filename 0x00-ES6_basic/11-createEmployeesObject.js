80042a3dc194325afb8b09ee41c4b89656dc0c02
mohamednouhi <mohamednoouuhi@gmail.com>
2024-04-23 16:54:08 +0100

    11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
-  let obj = {
-    [departmentName] : employees,
-  }
-  return obj
+  const obj = {
+    [departmentName]: employees,
+  };
+  return obj;
 }
-
