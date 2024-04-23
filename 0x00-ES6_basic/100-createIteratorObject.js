632877e7e1ea4063aa1bff353ebe3392c0fb4754
mohamednouhi <mohamednoouuhi@gmail.com>
2024-04-23 17:20:22 +0100

    13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,18 @@
 export default function createIteratorObject(report) {
   let employees = [];
+
+  // Extract all employee names from all departments
   for (let department in report.allEmployees) {
     employees.push(...report.allEmployees[department]);
   }
-  return employeesSymbol.iterator;
+
+  // Define a generator function to yield each employee name
+  function* employeeIterator() {
+    for (let employee of employees) {
+      yield employee;
+    }
+  }
+
+  // Return the iterator
+  return employeeIterator();
 }
